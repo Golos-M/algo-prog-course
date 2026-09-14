649630fb4fe1f253841f55659caeafe00a3d3c3d
Golos-M <golosm750@gmai.com>
2026-09-14 16:23:09 +0300

    Lab01 final: student visitka program

---
+++ Visitka/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+string Name = "Иванова Иван Иванович";
+﻿const string Name = "Иванова Иван Иванович";
+string group = "ИПС-251";
+int course = 2;
+string spec = "09.02.07";
+
+double total = (3 + 4 + 5) / 3;
+int week = 9;
+Console.WriteLine($"    ВИЗИТНАЯ КАРТОЧКА СТУДЕНТА");
+Console.WriteLine($"ФИО:            {Name}");
+Console.WriteLine($"Группа:         {group}");
+Console.WriteLine($"Курс:           {course}");
+Console.WriteLine($"Специальность:  {spec}");
+
+Console.WriteLine();
+Console.WriteLine($"");
+
+Console.WriteLine($"Ср. балл за 3 работы:        {total}");
+Console.WriteLine($"Стипендия положена (>= 4.0): True");
+
+Console.WriteLine($"");
+
+Console.WriteLine($"Учебных недель осталось в семестре: {week}");
