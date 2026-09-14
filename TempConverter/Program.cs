f76f04d92a167fcf2214331cc6801d0fa931db1a
Golos-M <golosm750@gmai.com>
2026-09-14 16:25:53 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!");
+﻿double celsius = 29;
+double F = celsius*9/5 + 32;
+const double Kel = 273.15;
+double K = celsius + Kel;
+
+Console.WriteLine($"{F} .... {K}");
